97b45854811f3f2fc4801b5c397dbfa86bedca12
李辰 <2095055676@qq.com>
2024-10-05 18:27:50 +0800

    feat: cors跨域

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import dbConnection from './src/Middleaware/db.js';
-import loggerMiddleware from './src/Middleaware/log.js';
+import cors from "cors";
+import dbConnection from "./src/Middleaware/db.js";
+import loggerMiddleware from "./src/Middleaware/log.js";
 // import router from './src/Router/index.js';
-import router from './src/Router/index.js';
+import router from "./src/Router/index.js";
 const app = express();
 // 连接数据库
 await dbConnection();
+// 跨域处理
+app.use(cors());
 // 日志中间件
 app.use(loggerMiddleware);
 app.use(express.json());
