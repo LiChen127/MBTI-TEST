97705ca8942c9b907963038f9b8e71e7b9b676e2
李辰 <2095055676@qq.com>
2024-10-11 08:57:37 +0800

    wig: stash

--- MBTI_TEST/src/main.js
+++ MBTI_TEST/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/global.scss';
 import router from './router/index';
+import 'element-plus/es/components/message/style/css';
+import { ElMessage } from 'element-plus';
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
